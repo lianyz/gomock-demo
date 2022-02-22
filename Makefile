9e3f21abff27c15c885ad09aa7eb679e0ff81985
lianyz <lianyz@email.cn>
2022-02-22 19:08:07 +0800

    增加go generate

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: gen
 gen:
 	mkdir -p mocks
-	mockgen -destination=mocks/mock_doer.go -package=mocks github.com/lianyz/gomock-demo/doer Doer,DoDo
+	go generate ./...
 
 .PHONY: test
 test:
--- doer/doer.go
+++ doer/doer.go
@@ -1,5 +1,7 @@
 package doer
 
+//go:generate mockgen -destination=../mocks/mock_doer.go -package=mocks github.com/lianyz/gomock-demo/doer Doer,DoDo
+
 type Doer interface {
 	DoSomething(int, string) error
 }
